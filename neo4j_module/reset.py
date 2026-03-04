3a958f447b93d55871063625b6b704b969bcf637
Moussa <mouss24.smail@gmail.com>
2026-03-04 18:52:49 +0100

    Fix: use env vars for Neo4j credentials

--- neo4j_module/reset.py
+++ neo4j_module/reset.py
@@ -1,9 +1,9 @@
+import os
 from neo4j import GraphDatabase
 
-NEO4J_URI = "bolt://localhost:7687"
-NEO4J_USER = "neo4j"
-NEO4J_PASSWORD = "Nassima94!!"  
-
+NEO4J_URI = os.getenv("NEO4J_URI", "bolt://localhost:7687")
+NEO4J_USER = os.getenv("NEO4J_USER", "neo4j") 
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD") # pas de valeur par défaut !
 
 class Neo4jReset:
 
