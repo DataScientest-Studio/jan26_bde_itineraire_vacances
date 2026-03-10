b1a4411cbee34eb656702220d0d38030fc84caf3
asiaud-83 <asiaud@free.fr>
2026-03-10 10:34:43 +0100

    ajout_docker_api

--- api/scripts/utils/db_connect.py
+++ api/scripts/utils/db_connect.py
@@ -24,3 +24,23 @@ def get_pg_conn():
         user=os.getenv("POSTGRES_USER"),
         password=os.getenv("POSTGRES_PASSWORD")
     )
+    
+def get_pg_conn_api():
+    """Crée et retourne une connexion PostgreSQL."""
+    return psycopg2.connect(
+        host=os.getenv("DB_PG_HOST"),
+        database=os.getenv("POSTGRES_DB"),
+        user=os.getenv("POSTGRES_USER"),
+        password=os.getenv("POSTGRES_PASSWORD")
+    )
+    
+def get_mongo_client_api():
+    """Crée et retourne une collection MongoDB."""
+    client = MongoClient(
+        host=os.getenv("DB_MDB_HOST"),
+        port=int(os.getenv("MONGO_PORT")),
+        username=os.getenv("MONGO_INITDB_ROOT_USERNAME"),
+        password=os.getenv("MONGO_INITDB_ROOT_PASSWORD")
+    )
+    db = client[os.getenv("MONGO_DB_NAME")]
+    return db[os.getenv("MONGO_COLLECTION_NAME")]
