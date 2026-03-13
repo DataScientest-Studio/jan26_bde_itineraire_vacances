ee57e650cfe46e12af1acdf1f3692bff00c9ed7c
Matthieu <matthieu.rotaru@icloud.com>
2026-03-13 15:13:26 +0100

    clean du code airflow

--- airflow/dags/tourisme_main_pipeline.py
+++ airflow/dags/tourisme_main_pipeline.py
@@ -4,7 +4,7 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 import requests
 
-# 1. Configuration des constantes
+# On récupère les variables d'environnement définies dans le .env
 API_URL = os.getenv("API_URL") 
 USERNAME = os.getenv("API_USER")
 PASSWORD = os.getenv("API_PWD")
@@ -22,13 +22,13 @@ def get_token():
     return token
 
 def run_batch(endpoint, **kwargs):
-    # 'ti' signifie Task Instance. Airflow l'injecte automatiquement via **kwargs.
+    # 'ti' pour Task Instance car Airflow l'injecte automatiquement via **kwargs.
     ti = kwargs['ti']
     
-    # On va chercher explicitement la valeur retournée par 'get_auth_token'
+    # On doit utiliser xcom car notre token est stocké ici (retour de la fonction)
     token_recupere = ti.xcom_pull(task_ids='get_auth_token')
     
-    print(f"Token récupéré via XCom : {token_recupere[:10]}...") # Sécurité : on n'affiche que le début
+    print(f"Token récupéré via XCom : {token_recupere[:10]}...")
     
     url = f"{API_URL}/{endpoint}"
     headers = {"Authorization": f"Bearer {token_recupere}"}
@@ -39,11 +39,11 @@ def run_batch(endpoint, **kwargs):
 # 2. Définition du DAG
 with DAG(
     dag_id="full_tourisme_pipeline",
-    # "0 8,20 * * *" signifie 8h00 et 20h00 tous les jours
+    # 2 fois par jours, mais on pourra le changer au besoin
     schedule_interval="0 8,20 * * *", 
-    start_date=datetime(2024, 1, 1),
-    catchup=False,
-    tags=['production', 'api']
+    start_date=datetime(2026, 1, 1),
+    catchup=False, #ici on doit mettre faux sinon airflow va relancer tous les runs dans le passé!!
+    tags=['api']
 ) as dag:
 
     # Tâche 0 : Authentification
@@ -77,5 +77,5 @@ with DAG(
         op_kwargs={"endpoint": "ppl_batch_4"}
     )
 
-    # 3. Définition de l'ordre d'exécution (le workflow)
+    # Définition de l'ordre du workflow
     auth_task >> task_1 >> task_2 >> task_3 >> task_4
